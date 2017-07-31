41f59e10433e82d1e06c73d5d1dfada20dedd517
DanielBallardP <daniel.pfanzagl@gmail.com>
2017-08-01 00:48:00 +0200

    modified app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import random
 app = Flask(__name__)
 
 # list of cat images
-images_ext = [
+images = [
     "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif",
     "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif",
     "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26390-1381844163-18.gif",
@@ -20,7 +20,7 @@ images_ext = [
 
 @app.route('/')
 def index():
-    url = random.choice(images_ext)
+    url = random.choice(images)
     return render_template('index.html', url=url)
 
 if __name__ == "__main__":
