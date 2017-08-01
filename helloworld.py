80324466047a5591c5bb82555a034cba3104cb3d
DanielBallardP <daniel.pfanzagl@gmail.com>
2017-08-02 00:59:48 +0200

    changed print output

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,20 @@
+import numpy as np
 numbers = [1,2,3,4,5]
 strings = ["test","blabla"]
 names = ["Daniel","Ann","Ellie"]
 
-for x in xrange(0,3):
+np_numbers = np.array(numbers)
+np_strings = np.array(strings)
+np_names = np.array(names)
+
+for x in range(0,3):
 	name = names[x]
 	print(name)
 	print("X is"+str(x))
 	x+1
 
-print(numbers)
-print(strings)
+print(np.mean(np_numbers))
+print(np_strings[3])
+print(np_names[:])
+print(type(np_names))
 print("The final name on the names list is %s" % name)
